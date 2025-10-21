e7ad66fce32624753072c5b19b3f9ba84afd31c9
Paulo Vitor <vitorkellylaura@gmail.com>
2025-10-21 10:20:46 -0300

    mudança de porta para o replit. 5000

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,8 @@ app.use(express.json());
 app.get('/', (req, res) => res.json({ ok: true, msg: 'API rodando' }));
 app.use('/', routes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
+
 
 async function start() {
   try {
